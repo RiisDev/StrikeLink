058986da7179d6587662112779b0b430b41403e7
Iris <39096994+RiisDev@users.noreply.github.com>
2025-12-18 04:58:59 -0700

    Initial Setup + Server Listener & Templates

---
+++ GSI/Parsing/PlayerStateParser.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CounterConnect.GSI.Parsing
+{
+	// Example class, non-functional
+	public sealed class PlayerStateParser : IGsiParser
+	{
+		public bool CanParse(JsonElement root) => root.TryGetProperty("player", out _);
+
+		public IGsiPayload Parse(JsonElement root)
+		{
+			JsonElement player = root.GetProperty("player");
+
+			return new PlayerStatePayload
+			{
+				Health = player.GetProperty("health").GetInt32(),
+				Armor = player.GetProperty("armor").GetInt32()
+			};
+		}
+	}
+}
