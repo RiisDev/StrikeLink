ceb71ab1929f7115cfd29a3836e9e0e298ff7a74
Iris <39096994+RiisDev@users.noreply.github.com>
2025-12-23 09:36:03 -0700

    RoundState

---
+++ GSI/Parsing/RoundStateParser.cs
@@ -0,0 +1,33 @@
+﻿using StrikeLink.GSI.ObjectStates;
+
+namespace StrikeLink.GSI.Parsing
+{
+	internal sealed class RoundStateParser : IGsiParser
+	{
+		public bool CanParse(JsonElement root) => root.TryGetProperty("round", out JsonElement playerElement) && playerElement.TryGetProperty("phase", out _);
+
+		public IGsiPayload Parse(JsonElement root)
+		{
+			JsonElement player = root.GetProperty("round");
+
+			return new RoundState(
+				Phase: Enum.TryParse(player.GetProperty("phase").GetString()!, true, out RoundPhase phase) ? phase : RoundPhase.Live,
+				WinTeam: player.TryGetProperty("win_team", out JsonElement winTeamElement) ? TeamParse(winTeamElement) : null,
+				BombState: player.TryGetProperty("bomb", out JsonElement bombElement) ? BombParse(bombElement) : null
+			);
+		}
+
+		private static Team TeamParse(JsonElement input)
+		{
+			string? value = input.GetString();
+			return value switch
+			{
+				"CT" => Team.CounterTerrorist,
+				"T" => Team.Terrorist,
+				_ => throw new NotImplementedException()
+			};
+		}
+
+		private static BombState BombParse(JsonElement input) => Enum.TryParse(input.GetString()!, true, out BombState state) ? state : BombState.Planted;
+	}
+}
