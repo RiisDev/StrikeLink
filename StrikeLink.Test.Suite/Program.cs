36d5bf275fe1bf4f7fe07ff5b2b890ffd90e59d1
Iris <39096994+RiisDev@users.noreply.github.com>
2025-12-22 19:06:38 -0700

    Win32 Helper

--- StrikeLink.Test.Suite/Program.cs
+++ StrikeLink.Test.Suite/Program.cs
@@ -1,10 +1,29 @@
-﻿using StrikeLink.GSI;
+﻿using StrikeLink.Extensions;
+using StrikeLink.GSI;
 using StrikeLink.Services;
 using StrikeLink.Services.Config;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Net;
 using System.Text;
 using System.Text.Json;
+
+Process cs2Proc = Process.GetProcessesByName("cs2")[0];
+
+_ = Task.Run(async () =>
+{
+	while (true)
+	{
+		await Task.Delay(250);
+
+		bool active = Win32.IsPrcoessActivated(cs2Proc);
+
+		Debug.WriteLine($"CS Active: {active}");
+
+		if (active)
+			Win32.SendLKey(Win32.VirtualKey.L);
+	}
+});
 
 object lockObj = new();
 ServerListener listen = new();
