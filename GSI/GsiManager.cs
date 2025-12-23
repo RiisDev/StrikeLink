c5122582e211642b700e26838b0db95a6c153890
Iris <39096994+RiisDev@users.noreply.github.com>
2025-12-22 17:03:20 -0700

    SteamService Impl

--- GSI/GsiManager.cs
+++ GSI/GsiManager.cs
@@ -1,56 +1,18 @@
-﻿using Microsoft.Win32;
+﻿using StrikeLink.Extensions;
+using StrikeLink.Services;
 using StrikeLink.Services.Config;
 using System.Net;
-using System.Runtime.InteropServices;
 
 namespace StrikeLink.GSI
 {
-	public static class GsiManager
+	internal static class GsiManager
 	{
-		internal static string GetSteamPath()
-		{
-			if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) throw new FileNotFoundException("Unable to automatically find steam, (OS_NOT_WINDOWS)");
-
-			using RegistryKey? key = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam");
-			if (key == null) throw new KeyNotFoundException($"{key} could not be found.");
-
-			object? value = key.GetValue("SteamPath");
-			return value as string ?? throw new KeyNotFoundException("Steam path key could not be found.");
-		}
-
-		internal static string GetCounterStrikePath(string steamPath)
-		{
-			string vdfPath = Path.Combine(steamPath, "steamapps", "libraryfolders.vdf");
-
-			if (!File.Exists(vdfPath)) throw new FileNotFoundException($"Failed to find libraryfolders.vdf at {vdfPath}");
-
-			ValveCfgReader reader = new(vdfPath);
-
-			foreach (KeyValuePair<string, ConfigNode> node in reader.Document.Root.EnumerateObject())
-			{
-				bool appsFound = node.Value.TryGetProperty("apps", out ConfigNode appsNode);
-				if (!appsFound) continue;
-
-				bool hasCs = appsNode.TryGetProperty("730", out ConfigNode _);
-				if (!hasCs) continue;
-
-				string path = node.Value.GetProperty("path").GetString();
-				string counterstrikeInstallPath = Path.Combine(path.Replace(@"\\", @"\", StringComparison.InvariantCulture), "steamapps", "common", "Counter-Strike Global Offensive");
-
-				return !Directory.Exists(counterstrikeInstallPath) ? throw new DirectoryNotFoundException($"{counterstrikeInstallPath} does not exist") : counterstrikeInstallPath;
-			}
-
-			throw new FileNotFoundException("Could not find Counter-Strike: Global Offensive installation in any Steam library folder.");
-		}
-
 		internal static async Task<(IPAddress, int)> GenerateGsiFile(IPAddress address, int port, string? steamPath = null)
 		{
-			string lookupPath = steamPath ?? GetSteamPath();
+			bool validPath = SteamService.TryGetGamePath(730, out string? counterPath);
 
-			if (!Directory.Exists(lookupPath))
-				throw new DirectoryNotFoundException($"{lookupPath} does not exist");
-
-			string counterPath = GetCounterStrikePath(lookupPath);
+			if (!validPath || counterPath.IsNullOrEmpty())
+				throw new DirectoryNotFoundException("Could not find CS:2 installation path.");
 
 			string cfgPath = Path.Combine(counterPath, "game", "csgo", "cfg");
 			string gsiCfg = Path.Combine(cfgPath, "gamestate_integration_strikelink.cfg");
